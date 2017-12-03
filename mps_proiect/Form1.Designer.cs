48e8584d3c9e45d2c7897e9acf7dabf07a3dadc0
amalia.frincu <amalia.frincu>
2017-12-03 22:38:52 +0200

    Comenzi de sistem implementate.

---
+++ mps_proiect/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace mps_proiect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(12, 267);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(134, 23);
+            this.btnEnable.TabIndex = 0;
+            this.btnEnable.Text = "Enable Voice Control";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Enabled = false;
+            this.btnDisable.Location = new System.Drawing.Point(256, 267);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(128, 23);
+            this.btnDisable.TabIndex = 1;
+            this.btnDisable.Text = "Disable Voice Control";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 1);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(372, 260);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "-Log-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 302);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ mps_proiect/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
+using Microsoft.Win32;
+
+namespace mps_proiect
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+      
+        public Form1()
+        {
+            InitializeComponent();
+            synthesizer.SelectVoiceByHints(VoiceGender.Female);
+        }
+
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
+        }
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            //start recognize multiple commands
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            btnDisable.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] {
+                "mona print my name",
+                "mona datetime please",
+                "mona ip address",
+                "mona internet connection",
+                "goodbye mona",
+                "mona battery life"
+            });
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+            
+        }
+
+        void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "mona print my name":
+                    richTextBox1.Text += "\nAmalia";
+                    break;
+                case "mona datetime please":
+                    //synthesizer.SpeakAsync(richTextBox1.SelectedText);
+                    synthesizer.SpeakAsync(DateTime.Now.ToString("yyyy, MM, dd, hh, mm"));
+                    break;
+                case "mona ip address":
+                    var host = Dns.GetHostEntry(Dns.GetHostName());
+                    string ipAddress = "";
+                    foreach (var ip in host.AddressList)
+                    {
+                        if (ip.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                             ipAddress = ip.ToString();
+                        }
+                    }
+                    if (ipAddress.Length != 0)
+                    {
+                        synthesizer.SpeakAsync("The ip address is " + ipAddress);
+                    } else
+                    {
+                        synthesizer.SpeakAsync("No network adapters with an IPv4 address in the system!");
+                    }
+                    break;
+                case "mona internet connection":
+                    try
+                    {
+                        Dns.GetHostEntry("www.google.com"); //using System.Net;
+                        synthesizer.SpeakAsync("Yes, you are connected to internet");
+                    }
+                    catch (SocketException ex)
+                    {
+                        synthesizer.SpeakAsync("It looks like you have problems with internet connectivity.");
+                    }
+                    break;
+                case "goodbye mona":
+                    synthesizer.SpeakAsync("Goodbye Amalia! Have a nice day!");
+                    System.Threading.Thread.Sleep(5000);
+                    Application.Exit();
+                    break;
+                case "mona battery life":
+                    String batterystatus;
+
+                    PowerStatus pwr = SystemInformation.PowerStatus;
+                    batterystatus = SystemInformation.PowerStatus.BatteryChargeStatus.ToString();
+
+                    synthesizer.SpeakAsync("battery charge status is" + batterystatus);
+                    break;
+            }
+        }
+    }
+}
